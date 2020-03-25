e48a26468e32e2be226d2b92c3d74f3a8065ae4a
kswon12 <kswon12@naver.com>
2020-03-25 17:44:02 +0900

    오류수정

--- src/test/java/com/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/book/springboot/web/HelloControllerTest.java
@@ -1,7 +1,6 @@
 package com.book.springboot.web;
 
 import com.book.springboot.config.auth.SecurityConfig;
-import org.graalvm.compiler.lir.CompositeValue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
