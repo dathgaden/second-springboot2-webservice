7c31905afabfb92b409016f6338938315d14b72f
kswon12 <kswon12@naver.com>
2020-03-25 17:16:28 +0900

    Initial commit

---
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -0,0 +1,40 @@
+package com.book.springboot.web;
+
+import com.book.springboot.config.auth.LoginUser;
+import com.book.springboot.config.auth.dto.SessionUser;
+import com.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("posts", postsService.findById(id));
+        return "posts-update";
+    }
+}
